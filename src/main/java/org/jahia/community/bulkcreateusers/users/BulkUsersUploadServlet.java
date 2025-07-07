d1bff6c8d8121b2571301b183beb5c376ab81ee9
ccheriyan <ccheriyan@jahia.com>
2025-07-07 16:06:30 -0400

    Update UI from webflow to React

---
+++ src/main/java/org/jahia/community/bulkcreateusers/users/BulkUsersUploadServlet.java
@@ -0,0 +1,74 @@
+package org.jahia.community.bulkcreateusers.users;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.jahia.community.bulkcreateusers.users.management.CsvFile;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jcr.RepositoryException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@Component(
+        service = {javax.servlet.http.HttpServlet.class, javax.servlet.Servlet.class},
+        property = {"alias=/bulk-users-upload", "osgi.http.whiteboard.servlet.asyncSupported=true"},
+        immediate = true
+)
+@MultipartConfig
+public class BulkUsersUploadServlet extends HttpServlet {
+
+    private static final Logger logger = LoggerFactory.getLogger(BulkUsersUploadServlet.class);
+
+    @Reference
+    private UsersHandler usersHandler;
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+
+        if (!ServletFileUpload.isMultipartContent(req)) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Not a multipart request");
+            return;
+        }
+
+        try {
+            String siteKey = null;
+            CsvFile csvFile = new CsvFile();
+            List<FileItem> items = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(req);
+
+            for (FileItem item : items) {
+                if (item.isFormField()) {
+                    switch (item.getFieldName()) {
+                        case "siteKey":
+                            siteKey = item.getString().isEmpty() ? null : item.getString();
+                            break;
+                        default:
+                            csvFile.setCsvSeparator(item.getString() != null ? item.getString() : ",");
+                    }
+                } else {
+                    csvFile.setCsvFile(item.getInputStream());
+                }
+            }
+
+            boolean success = usersHandler.bulkAddUser(csvFile, siteKey);
+            resp.setStatus(success ? HttpServletResponse.SC_OK : HttpServletResponse.SC_BAD_REQUEST);
+            resp.getWriter().write("{\"success\": " + success + ", \"message\": \"" +
+                    (success ? "Users created successfully." : "Some errors occurred during user creation.") + "\"}");
+
+        } catch (FileUploadException | RepositoryException e) {
+            logger.error("Error during file upload or repository operation", e);
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "User creation failed");
+        }
+    }
+}
--- src/main/java/org/jahia/community/bulkcreateusers/users/management/CsvFile.java
+++ src/main/java/org/jahia/community/bulkcreateusers/users/management/CsvFile.java
@@ -1,18 +1,18 @@
 package org.jahia.community.bulkcreateusers.users.management;
 
-import java.io.Serializable;
-import org.jahia.utils.i18n.Messages;
-import org.springframework.binding.message.MessageBuilder;
-import org.springframework.binding.validation.ValidationContext;
-import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.web.multipart.MultipartFile;
+import java.io.InputStream;
 
-public class CsvFile implements Serializable{
-
-    private static final long serialVersionUID = 5506459343721460018L;
-
+public class CsvFile {
+    private InputStream csvFile;
     private String csvSeparator;
-    private MultipartFile csvFile;
+
+    public InputStream getCsvFile() {
+        return csvFile;
+    }
+
+    public void setCsvFile(InputStream csvFile) {
+        this.csvFile = csvFile;
+    }
 
     public String getCsvSeparator() {
         return csvSeparator;
@@ -20,20 +20,5 @@ public class CsvFile implements Serializable{
 
     public void setCsvSeparator(String csvSeparator) {
         this.csvSeparator = csvSeparator;
-    }
-
-    public MultipartFile getCsvFile() {
-        return csvFile;
-    }
-
-    public void setCsvFile(MultipartFile csvFile) {
-        this.csvFile = csvFile;
-    }
-    
-    public void validateBulkCreateUser(ValidationContext context) {
-        if (csvFile == null || csvFile.isEmpty()) {
-            context.getMessageContext().addMessage(new MessageBuilder().error().source("csvFile")
-                    .defaultText(Messages.get("resources.JahiaSiteSettings", "siteSettings.users.bulk.errors.missing.import", LocaleContextHolder.getLocale())).build());
-        }
     }
 }
